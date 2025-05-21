71a4cff1f624f22953640ca6622e5fd65b64bea0
Fouxi <foucault.wattinne@gmail.com>
2025-05-21 22:39:11 +0200

    Android c'est dans la poche

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,40 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import tailwindcss from '@tailwindcss/vite';
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {hmr: true},
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Centraliz - Gérer sa vie à Centrale",
+        short_name: "Centraliz",
+        description: "Centraliz - Gérer sa vie à Centrale",
+        start_url: "/",
+        orientation: "portrait",
+        icons: [
+          {
+            src: "/favicon/web-app-manifest-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/favicon/web-app-manifest-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        theme_color: "#fbfbfb",
+        background_color: "#4267ce",
+        display: "standalone",
+      },
+    }),
+  ],
+  server: { hmr: true },
 });
